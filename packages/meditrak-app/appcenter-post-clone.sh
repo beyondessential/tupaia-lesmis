6acc849286d520a0806cb343c1412ad9b2dbf4bf
billli0 <lbit123@outlook.com>
2022-02-11 14:25:44 +1100

    test

--- packages/meditrak-app/appcenter-post-clone.sh
+++ packages/meditrak-app/appcenter-post-clone.sh
@@ -11,6 +11,14 @@ env | grep "USER-DEFINED_.*" | awk -F "USER-DEFINED_" '{print $2}' > .env
 ## To manage that, we here manually install external deps, build internal deps, then redirect the
 ## package.json entries to the internal filesystem
 
+# Required nodeJS version
+NODE_VERSION=12.18.3
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+
 # move to the root folder
 cd ../..
 
